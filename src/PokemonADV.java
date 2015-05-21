daf35a1217c249748d7c91c51bc9ba81fa8ff162
Ali <toalisaeedmalik@gmail.com>
2015-05-21 12:32:29 -0400

    added basic intro

--- src/PokemonADV.java
+++ src/PokemonADV.java
@@ -3,7 +3,15 @@ public class PokemonADV {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String player,charmander, squirtle, bulbasaur;
+		
+		System.out.println("Hello world!");
+		System.out.println("Glad to meet you!");
+		System.out.println("Welcome to the world of pokemon");
+		System.out.println("My name is Oak!");
+		System.out.println("This world...");
+		System.out.println("...is inhabited");
+		
 	}
 
 }
